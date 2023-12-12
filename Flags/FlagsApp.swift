ecb48ce3d96a29197de4a1f6e0ff9564570379db
neshwa.parveen <neshwa.parveen@chillarcards.com>
2023-12-12 15:56:55 +0530

    Initial Commit

---
+++ Flags/FlagsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagsApp.swift
+//  Flags
+//
+//  Created by Neshwa on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct FlagsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
